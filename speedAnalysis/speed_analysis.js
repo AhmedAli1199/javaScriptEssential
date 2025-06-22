7121bca9a0656c7592cf332ca21cd1e5eb5bd50c
AhmedAli1199 <i220825@nu.edu.pk>
2025-06-22 10:07:52 -0400

    Added total length for user typed text

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -36,6 +36,8 @@ function endTest(){
     // Split using regex to count words
     var typedWords = userTypedText.split(/\s+/).filter(function (word){ return word !== ""; }).length;
 
+    var totalLength = userTypedText.length;
+
     var wpm = 0;
 
     if(timeElapsed !== 0 && !isNaN(typedWords))
@@ -46,6 +48,7 @@ function endTest(){
     //Display results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML =  "<h2>Typing Test Results:</h2>" +
+    "<p>Total Length: " + totalLength + "</p>" +
     "<p>Words Typed: " + typedWords + "</p>" +
     "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
     "<p>Words Per Minute (WPM): " + wpm + "</p>";
